081c324c3247c3a395e676e76c3b1f6302cb9766
Viktor Kindrat <kindratvictor07@gmail.com>
2023-06-16 16:36:57 +0300

    Rules page setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import GoodsContainer from "./components/GoodsContainer/GoodsContainer";
+import Rules from "./components/Rules/Rules";
 
 function App() {
   return (
     <div className="wrap">
       <Header/>
       <GoodsContainer/>
+      <Rules/>
       <Footer/>
     </div>
   );
