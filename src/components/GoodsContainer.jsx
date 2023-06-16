312bbf111656357b0c0a3b6471a1c3c912656d2c
Viktor Kindrat <kindratvictor07@gmail.com>
2023-06-16 16:02:59 +0300

    add 8 objects in goods array

--- src/components/GoodsContainer.jsx
+++ src/components/GoodsContainer.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import Goods from './Goods';
-const goods = [
-  { title: "Крісло", price: 300, image: "https://cdn1.jysk.com/getimage/wd2.medium/185422", color: "grey", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec.", sale: false, count: 2},
-  { title: "Стілець", price: 320, image: "https://economtochka.com.ua/image/cache/catalog/samba/800x1000px_Vensan_plus_wood_alu_1-007_KL306_34-800x800.jpg", color: "orange", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam.", sale: true, count: 5}];
+import goods from "../goods.json"
+
 export default function GoodsContainer() {
   return (
      <div className="GoodsContainer">
